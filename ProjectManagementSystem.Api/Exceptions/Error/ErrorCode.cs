3ae3329d064c5821e1f50372244994af15aa36a1
MoustafaMohamad <moustafamohamad.2020@azhar.edu.eg>
2024-09-23 19:40:09 +0300

    add middleware

--- ProjectManagementSystem.Api/Exceptions/Error/ErrorCode.cs
+++ ProjectManagementSystem.Api/Exceptions/Error/ErrorCode.cs
@@ -4,6 +4,7 @@ namespace ProjectManagementSystem.Api.Exceptions.Error;
 public enum ErrorCode
 {
     NoError = 0,
+    UnKnown,
     BadRequest = 400,
     UareNotAuthorized = 401,
     ResourceNotFound = 404, 
---
+++ ProjectManagementSystem.Api/Middlewares/GlobalErrorHandlerMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using ProjectManagementSystem.Api.Exceptions.Error;
+using ProjectManagementSystem.Api.Exceptions;
+using ProjectManagementSystem.Api.ViewModels.ResultViewModel;
+
+namespace ProjectManagementSystem.Api.Middlewares
+{
+    public class GlobalErrorHandlerMiddleware
+    {
+        RequestDelegate _next;
+
+        public GlobalErrorHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                string message = "Error Occured";
+                ErrorCode errorCode = ErrorCode.UnKnown;
+
+                if (ex is BusinessException businessException)
+                {
+                    message = businessException.Message;
+                    errorCode = businessException.ErrorCode;
+                }
+
+
+                var result = ResultViewModel<bool>.Failure(errorCode, message);
+
+                await context.Response.WriteAsJsonAsync(result);
+            }
+        }
+    }
+}
