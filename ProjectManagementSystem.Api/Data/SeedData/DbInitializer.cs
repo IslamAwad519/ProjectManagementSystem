9ae4131e2c23b43d856d8e4cb898ead5ef92ba8a
BassantAdell <bassatadel602@gmail.com>
2024-09-25 07:53:59 +0300

    DashBoard

---
+++ ProjectManagementSystem.Api/Data/SeedData/DbInitializer.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using ProjectManagementSystem.Api.Helpers;
+using ProjectManagementSystem.Api.Models;
+
+namespace ProjectManagementSystem.Api.Data.SeedData;
+
+public static class DbInitializer
+{
+    public static async Task Initialize(
+        UserManager<ApplicationUser> userManager,
+        RoleManager<IdentityRole> roleManager,
+        ApplicationDbContext context)
+    {
+        if (!await roleManager.RoleExistsAsync(AppRoles.Admin))
+        {
+            await roleManager.CreateAsync(new IdentityRole(AppRoles.Admin));
+            await roleManager.CreateAsync(new IdentityRole(AppRoles.Manager));
+            await roleManager.CreateAsync(new IdentityRole(AppRoles.User));
+
+            var admin = new ApplicationUser
+            {
+                UserName = "admin@test.com",
+                Email = "admin@test.com",
+                PhoneNumber = "01004255366",
+            };
+            await userManager.CreateAsync(admin, "Password1!");
+            await userManager.AddToRoleAsync(admin, AppRoles.Admin);
+
+            var manager = new ApplicationUser
+            {
+                UserName = "manager@test.com",
+                Email = "manager@test.com",
+                PhoneNumber = "01010157275",
+            };
+            await userManager.CreateAsync(manager, "Password1!");
+            await userManager.AddToRoleAsync(manager, AppRoles.Manager);
+        }
+    }
+}
